e66f3d9bdd9c8d6ecf7e040e9dcbd0d5a42c2645
gongice <gshiwen@gmail.com>
2016-02-17 10:58:42 +0800

    modified source code

--- src/main/java/org/gongice/util/TestLoadConf.java
+++ src/main/java/org/gongice/util/TestLoadConf.java
@@ -1,14 +1,14 @@
 package org.gongice.util;  
 
 import java.io.IOException;
-import org.gongice.util.config.ConfLoader;
+import org.gongice.util.config.ConfigLoader;
 import backtype.storm.Config;
 
 public class TestLoadConf {
 	public static void main(String[] args) throws IOException {
-		Config conf = new ConfLoader()
+		Config conf = new ConfigLoader()
 				.loadStormConfig("E:\\Workstation\\Github\\yaml-config\\conf\\event-conf-production.yaml");
-		ConfLoader.print(conf);
+		ConfigLoader.print(conf);
 		
 	}
 }
